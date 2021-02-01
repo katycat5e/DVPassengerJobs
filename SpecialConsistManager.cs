046ca4e2fa3638856cc7149d792969b7c095d6f1
Katy Fox <katycat5e@gmail.com>
2021-01-31 21:46:16 -0500

    Add special/named train config files

---
+++ SpecialConsistManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PassengerJobsMod
+{
+    public static class SpecialConsistManager
+    {
+        public static readonly List<SpecialTrain> TrainDefinitions = new List<SpecialTrain>();
+
+        private static readonly XmlSerializer serializer = new XmlSerializer(typeof(SpecialTrainConfig));
+
+        public static void LoadConfig( string path )
+        {
+            using( var fileStream = new FileStream(path, FileMode.Open) )
+            {
+                if( serializer.Deserialize(fileStream) is SpecialTrainConfig fileData )
+                {
+                    foreach( var train in fileData.Trains )
+                    {
+                        if( train.CheckValidity(out string result) )
+                        {
+                            TrainDefinitions.Add(train);
+                        }
+                        else
+                        {
+                            PassengerJobs.ModEntry.Logger.Warning($"Error in special train config: {result}, in {path}");
+                        }
+                    }
+                }
+                else
+                {
+                    PassengerJobs.ModEntry.Logger.Warning("Failed to load special train config " + path);
+                }
+            }
+        }
+
+        private static readonly Random rand = new Random();
+        public static SpecialTrain GetTrainForRoute( string startId, string destId )
+        {
+            List<SpecialTrain> matches = TrainDefinitions.Where(train => train.IsAllowedOnRoute(startId, destId)).ToList();
+
+            return (matches.Count > 0) ? matches.ChooseOne(rand) : null;
+        }
+    }
+}
---
+++ SpecialTrainConfig.cs
@@ -0,0 +1,117 @@
+﻿using DV.Logic.Job;
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace PassengerJobsMod
+{
+    public class SpecialTrainConfig
+    {
+        [XmlElement(ElementName = "Train")]
+        public SpecialTrain[] Trains;
+    }
+
+    public class SpecialTrain
+    {
+        public string Name = null;
+        public string Skin = null;
+
+        [XmlAttribute("CarType")]
+        public string CarTypeString = null;
+
+        [XmlIgnore]
+        public TrainCarType CarType;
+
+        [XmlAttribute("Routes")]
+        public string RouteString = null;
+
+        [XmlIgnore]
+        public bool AllowAnyRoute = false;
+
+        [XmlIgnore]
+        public StationsChainData[] Routes = null;
+
+        public bool IsAllowedOnRoute( string start, string end )
+        {
+            if( AllowAnyRoute ) return true;
+            
+            foreach( var route in Routes )
+            {
+                if( route.chainOriginYardId.Equals(start) && route.chainDestinationYardId.Equals(end) ) return true;
+            }
+
+            return false;
+        }
+
+        private bool ParseRoutes()
+        {
+            if( string.IsNullOrWhiteSpace(RouteString) || "*".Equals(RouteString) )
+            {
+                AllowAnyRoute = true;
+                return true;
+            }
+
+            string[] routePairs = RouteString.Split(',');
+            Routes = new StationsChainData[routePairs.Length];
+
+            for( int i = 0; i < routePairs.Length; i++ )
+            {
+                string[] stations = routePairs[i].ToUpper().Split('-');
+
+                if( (stations.Length == 2) &&
+                    SingletonBehaviour<LogicController>.Instance.YardIdToStationController.TryGetValue(stations[0], out _) &&
+                    SingletonBehaviour<LogicController>.Instance.YardIdToStationController.TryGetValue(stations[1], out _) )
+                {
+                    Routes[i] = new StationsChainData(stations[0], stations[1]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool CheckValidity( out string message )
+        {
+            const StringComparison comp = StringComparison.CurrentCultureIgnoreCase;
+            message = null;
+
+            // cartype must be red, green, or blue
+            if( "red".Equals(CarTypeString, comp) )
+            {
+                CarType = TrainCarType.PassengerRed;
+            }
+            else if( "green".Equals(CarTypeString, comp) )
+            {
+                CarType = TrainCarType.PassengerGreen;
+            }
+            else if( "blue".Equals(CarTypeString, comp) )
+            {
+                CarType = TrainCarType.PassengerBlue;
+            }
+            else
+            {
+                message = $"Invalid car type {CarType}";
+                return false;
+            }
+
+            // check that a skin is given
+            if( string.IsNullOrWhiteSpace(Skin) )
+            {
+                message = "No skin specified";
+                return false;
+            }
+
+            // check that the routes are okay
+            if( !ParseRoutes() )
+            {
+                message = "Invalid route specification";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
