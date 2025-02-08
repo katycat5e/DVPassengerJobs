3b2899cd69f174ec93615975aa0b0754c5417bc8
AMacro <andrew@andrewcraigmackenzie.com>
2025-02-09 00:24:10 +1000

    Add custom station fast travel destination

---
+++ PassengerJobs/Platforms/RuralStationFastTravelDestination.cs
@@ -0,0 +1,27 @@
+﻿using DV.Teleporters;
+using UnityEngine;
+
+namespace PassengerJobs.Platforms;
+
+public class RuralStationFastTravelDestination : StationFastTravelDestination
+{
+    public string StationName { get; private set; } = string.Empty;
+    public string ID { get; private set; } = string.Empty;
+
+
+    public override string MarkerName
+    {
+        get
+        {
+            return StationName;
+        }
+    }
+
+    public void Init(Transform teleportAnchor, Transform markerAnchor, string localisedStationName, string id)
+    {
+        playerTeleportAnchor = teleportAnchor;
+        mapMarkerAnchor = markerAnchor;
+        StationName = localisedStationName;
+        ID = id;
+    }
+}
