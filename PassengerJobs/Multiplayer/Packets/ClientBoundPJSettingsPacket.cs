ff7e8b02a808517fc24ae80e18ab8f03b8340ee4
Macka <andrew@andrewcraigmackenzie.com>
2025-08-30 14:09:14 +1000

    Add client-bound packets for PJ settings and station data
    Introduces ClientBoundPJSettingsPacket and ClientBoundPJStationDataPacket classes for multiplayer support. These packets handle the serialization and deserialization of passenger job settings and station data, including city and rural stations, translations, and sign locations.

---
+++ PassengerJobs/Multiplayer/Packets/ClientBoundPJSettingsPacket.cs
@@ -0,0 +1,14 @@
+﻿using MPAPI.Interfaces.Packets;
+using UnityEngine;
+using static PassengerJobs.PJModSettings;
+
+namespace PassengerJobs.Multiplayer.Packets;
+
+public class ClientBoundPJSettingsPacket : IPacket
+{
+    public bool UseCustomWages {get; set; }
+    public CoachLightMode CoachLights { get; set; }
+    public bool UseCustomCoachLightColour { get; set; }
+    public Color CustomCoachLightColour { get; set; }
+    public bool CoachLightsRequirePower { get; set; }
+}
