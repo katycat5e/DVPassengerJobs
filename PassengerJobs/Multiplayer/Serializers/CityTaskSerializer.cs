6cac1dc1155a3025127f6a50df7803804dcc1900
Macka <andrew@andrewcraigmackenzie.com>
2025-08-31 11:54:01 +1000

    Refactor CityLoadingTaskData serialisation
    Fixed serialisation and deserialisation of warehouse destination as no ID was available

--- PassengerJobs/Multiplayer/Serializers/CityTaskSerializer.cs
+++ PassengerJobs/Multiplayer/Serializers/CityTaskSerializer.cs
@@ -15,18 +15,24 @@ public class CityLoadingTaskData : TaskNetworkData<CityLoadingTaskData>
 {
     public ushort[]? CarNetIDs { get; set; }
     public WarehouseTaskType WarehouseTaskType { get; set; }
-    public string WarehouseMachineId { get; set; } = string.Empty;
+    public WarehouseMachine? WarehouseMachine { get; set; }
     public CargoType CargoType { get; set; }
     public float CargoAmount { get; set; }
     public bool ReadyForMachine { get; set; }
 
     public override void Serialize(BinaryWriter writer)
     {
+        //PJMain.Log($"Serializing CityLoadingTaskData.\r\n\tCarNetIds: [{string.Join(", ", CarNetIDs)}]\r\n\tWarehouseTaskType: {WarehouseTaskType}\r\n\tWarehouseMachineTrack: {WarehouseMachine?.WarehouseTrack?.ID}\r\n\tCargoType: {CargoType}\r\n\tCargoAmount: {CargoAmount}\r\n\tReadyForMachine: {ReadyForMachine}");
+
         SerializeCommon(writer);
 
         writer.WriteUShortArray(CarNetIDs);
         writer.Write((byte)WarehouseTaskType);
-        writer.Write(WarehouseMachineId ?? string.Empty);
+
+        var track = WarehouseMachine?.WarehouseTrack?.RailTrack() ?? null;
+        MultiplayerAPI.Instance.TryGetNetId(track, out ushort trackNetId);
+
+        writer.Write(trackNetId);
         writer.Write((int)CargoType);
         writer.Write(CargoAmount);
         writer.Write(ReadyForMachine);
@@ -35,17 +41,37 @@ public class CityLoadingTaskData : TaskNetworkData<CityLoadingTaskData>
     public override void Deserialize(BinaryReader reader)
     {
         DeserializeCommon(reader);
-        
+
         CarNetIDs = reader.ReadUShortArray();
         WarehouseTaskType = (WarehouseTaskType)reader.ReadByte();
-        WarehouseMachineId = reader.ReadString();
+
+        ushort trackNetId = reader.ReadUInt16();
+        if (!MultiplayerAPI.Instance.TryGetObjectFromNetId(trackNetId, out RailTrack? WarehouseMachineTrack) || !WarehouseMachineTrack)
+            throw new Exception($"Failed to deserialise CityLoadingTaskData for trackNetId {trackNetId}, RailTrack was not found");
+
+        var track = WarehouseMachineTrack.LogicTrack()?.ID?.FullDisplayID;
+        if (string.IsNullOrEmpty(track))
+            throw new Exception($"Failed to deserialise CityLoadingTaskData for trackNetId {trackNetId}, LogicTrack was not found");
+
+        if (!PlatformController.TryGetControllerForTrack(track, out var controller) || !controller)
+            throw new Exception($"Failed to deserialise CityLoadingTaskData for trackNetId {trackNetId}, PlatformController was not found");
+
+        if (controller!.Platform is not StationPlatformWrapper stationPlatform)
+            throw new Exception($"Failed to deserialise CityLoadingTaskData for trackNetId {trackNetId}, Platform is not a StationPlatform");
+
+        WarehouseMachine = stationPlatform.Warehouse;
+        
         CargoType = (CargoType)reader.ReadInt32();
         CargoAmount = reader.ReadSingle();
         ReadyForMachine = reader.ReadBoolean();
+
+        //PJMain.Log($"Deserializing CityLoadingTaskData.\r\n\tCarNetIds: [{string.Join(", ", CarNetIDs)}]\r\n\tWarehouseTaskType: {WarehouseTaskType}\r\n\tWarehouseMachineTrack: {WarehouseMachineTrack?.name}\r\n\tCargoType: {CargoType}\r\n\tCargoAmount: {CargoAmount}\r\n\tReadyForMachine: {ReadyForMachine}");
     }
 
     public override CityLoadingTaskData FromTask(Task task)
     {
+        //PJMain.Log($"CityLoadingTaskData.FromTask({task.Job.ID})");
+
         if (task is not CityLoadingTask cityLoadingTask)
             throw new ArgumentException("Task is not a CityLoadingTask");
 
@@ -61,8 +87,10 @@ public class CityLoadingTaskData : TaskNetworkData<CityLoadingTaskData>
             )
             .ToArray();
 
+        //PJMain.Log($"CityLoadingTaskData.FromTask({task.Job.ID}) WarehouseMachine.WarehouseTrack: {cityLoadingTask.warehouseMachine.WarehouseTrack.ID}, WarehouseMachine.ID: {cityLoadingTask.warehouseMachine.ID}");
+
         WarehouseTaskType = cityLoadingTask.warehouseTaskType;
-        WarehouseMachineId = cityLoadingTask.warehouseMachine.ID;
+        WarehouseMachine = cityLoadingTask.warehouseMachine;
         CargoType = cityLoadingTask.cargoType;
         CargoAmount = cityLoadingTask.cargoAmount;
         ReadyForMachine = cityLoadingTask.readyForMachine;
@@ -78,17 +106,20 @@ public class CityLoadingTaskData : TaskNetworkData<CityLoadingTaskData>
             .Select(car => car!.logicCar)
             .ToList();
 
+        if (WarehouseMachine == null)
+            throw new Exception($"Failed to convert CityLoadingTaskData to Task, WarehouseMachine is null!");
 
-        CityLoadingTask newTask = new CityLoadingTask
+        CityLoadingTask newTask = new
         (
            cars,
            WarehouseTaskType,
-           JobSaveManager.Instance.GetWarehouseMachineWithId(WarehouseMachineId),
+           WarehouseMachine,
            CargoType,
            CargoAmount
-        );
-
-        newTask.readyForMachine = ReadyForMachine;
+        )
+        {
+            readyForMachine = ReadyForMachine
+        };
 
         return newTask;
     }
